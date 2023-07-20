3dab55c71d7d12db302dfaade9f3e4ff0cb835c2
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-07-20 00:07:54 -0500

    updating files

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,14 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // Function for authenticating routes
-  authMiddleware: function (req, res, next) {
-    // Allows token to be sent via req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,20 +14,17 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // Verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch (err) {
+    } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'Invalid token!' });
     }
 
-    // Proceed to the next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
